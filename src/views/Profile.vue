5fa856d19c5445feaf4cae1284b200c7ed8bf7c7
Patryk Kaca <patrykaca@gmail.com>
2021-09-06 23:20:55 +0200

    edit profile view part2

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,14 +2,14 @@
   <v-container class="container">
     <!--    {{ JSON.stringify(this.user) }}-->
     <Spinner
-      v-if="loading"
+        v-if="loading"
     />
     <div
-      v-else
-      class="pa-8"
+        v-else
+        class="pa-8"
     >
       <div
-        v-if="!editMode"
+          v-if="!editMode"
       >
         <div class="text-h6">
           {{ $t('general.first-name') }}:
@@ -29,158 +29,95 @@
         <div class="mb-4 text-h5 font-weight-bold ml-16 pl-11">
           {{ user.description }}
         </div>
-<!--        <v-row>-->
-<!--          <v-col cols="12" md="6">-->
-<!--            <v-text-field-->
-<!--                v-model.trim="editedUser.firstName"-->
-<!--                :label="$t('general.first-name')"-->
-<!--                outlined-->
-<!--                readonly-->
-<!--            />-->
-<!--          </v-col>-->
-<!--        </v-row>-->
-<!--        <v-row>-->
-<!--          <v-col cols="12" md="6">-->
-<!--            <v-text-field-->
-<!--                v-model.trim="editedUser.lastName"-->
-<!--                :label="$t('general.first-name')"-->
-<!--                outlined-->
-<!--                readonly-->
-<!--            />-->
-<!--          </v-col>-->
-<!--        </v-row>-->
-<!--        <v-row>-->
-<!--          <v-col cols="12" md="6">-->
-<!--            <v-textarea-->
-<!--                v-model.trim="editedUser.description"-->
-<!--                :label="$t('general.first-name')"-->
-<!--                outlined-->
-<!--                readonly-->
-<!--            />-->
-<!--          </v-col>-->
-<!--        </v-row>-->
       </div>
       <div v-else>
         <div class="text-h6 ">
           {{ $t('general.first-name') }}:
         </div>
         <div class="mb-2 text-h5 font-weight-bold ml-16 pl-11">
-        <v-row>
-          <v-col cols="12" md="6">
-            <v-text-field
-                v-model.trim="editedUser.firstName"
-                outlined
-            />
-          </v-col>
-        </v-row>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field
+                  v-model.trim="editedUser.firstName"
+                  outlined
+              />
+            </v-col>
+          </v-row>
         </div>
         <div class="text-h6">
           {{ $t('general.last-name') }}:
         </div>
         <div class="mb-2 text-h5 font-weight-bold ml-16 pl-11">
-        <v-row>
-          <v-col cols="12" md="6">
-            <v-text-field
-                v-model.trim="editedUser.lastName"
-                outlined
-            />
-          </v-col>
-        </v-row>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field
+                  v-model.trim="editedUser.lastName"
+                  outlined
+              />
+            </v-col>
+          </v-row>
         </div>
         <div class="text-h6">
           {{ $t('general.description') }}:
         </div>
         <div class="mb-2 text-h5 font-weight-bold ml-16 pl-11">
-        <v-row>
-          <v-col
-              cols="12"
-              md="6"
-          >
-           <v-textarea
-                v-model.trim="editedUser.description"
-                outlined
-           />
-          </v-col>
-        </v-row>
+          <v-row>
+            <v-col
+                cols="12"
+                md="6"
+            >
+              <v-textarea
+                  v-model.trim="editedUser.description"
+                  outlined
+              />
+            </v-col>
+          </v-row>
         </div>
       </div>
       <v-btn
-        v-if="editPossible && !editMode"
-        class="v-btn v-btn--text theme--dark v-size--default"
-        @click="editMode = !editMode"
+          v-if="editPossible && !editMode"
+          class="v-btn v-btn--text theme--dark v-size--default"
+          @click="editMode = !editMode"
       >
         EDIT
       </v-btn>
-      <div class="justify-space-between py-3">
-        <v-btn
-          v-if="editPossible && editMode"
-          class="v-btn"
-          @click="cancelBtn()"
-        >
-           CANCEL
-        </v-btn>
-        <v-btn
-          v-if="editPossible && editMode"
-          class="v-btn primary pull-right"
-          @click="editUser"
-        >
-          SAVE
-        </v-btn>
+      <div class="ml-16 pl-11 mr-16 d-flex justify-space-between">
+      <v-row>
+        <v-col class="d-flex justify-space-between">
+          <div class="d-flex">
+            <v-btn
+                v-if="editPossible && editMode"
+                class="v-btn mr-16"
+                @click="cancelBtn()"
+            >
+              CANCEL
+            </v-btn>
+            <v-btn
+                v-if="editPossible && editMode"
+                class="v-btn primary ml-16"
+                @click="editUser"
+            >
+              SAVE
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
       </div>
-      <!--    <v-row class="mb-0">-->
-      <!--      <v-col-->
-      <!--          cols="12"-->
-      <!--          sm="8"-->
-      <!--      >-->
-      <!--        <div class="text-h4">-->
-      <!--          <header class="jumbotron">-->
-      <!--            <h3>-->
-      <!--              <strong>{{ displayName }}</strong> Profile-->
-      <!--            </h3>-->
-      <!--          </header>-->
-      <!--        </div>-->
-      <!--      </v-col>-->
-      <!--    </v-row>-->
-      <!--    <div class="d-flex align-center">-->
-      <!--      <v-divider class="mr-3"/>-->
-      <!--    </div>-->
-
-      <!--    <v-text-field-->
-      <!--        v-model.trim="edited.lastName"-->
-      <!--        :label="$t('general.last-name')"-->
-      <!--        outlined-->
-      <!--    />-->
-      <!--    <v-text-field-->
-      <!--        v-model.trim="edited.firstName"-->
-      <!--        :label="$t('general.first-name')"-->
-      <!--        outlined-->
-      <!--    />-->
-      <!--    <v-text-field-->
-      <!--        v-model.trim="edited.description"-->
-      <!--        :label="$t('general.description')"-->
-      <!--        outlined-->
-      <!--    />-->
-      <!--    <v-btn-->
-      <!--        @click="editUser"-->
-      <!--    >-->
-      <!--      Edit-->
-      <!--    </v-btn>-->
-
     </div>
   </v-container>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import {mapGetters} from 'vuex';
 import UserService from '@/services/http/user.service';
 import i18n from '@/locales/i18n';
-import { NOTIFICATION_TYPES } from '@/store/modules/notification';
+import {NOTIFICATION_TYPES} from '@/store/modules/notification';
 import Spinner from '@/components/shared/Spinner';
-import { required } from 'vuelidate/lib/validators';
+import {maxLength, minLength, required} from 'vuelidate/lib/validators';
 
 export default {
   name: 'Profile',
-  components: { Spinner },
+  components: {Spinner},
   data() {
     return {
       user: undefined,
@@ -198,6 +135,7 @@ export default {
     ...mapGetters({
       displayName: 'user/displayName',
       currentUserId: 'user/userId',
+      currentUser: 'user/user'
     }),
     editPossible() {
       return this.currentUserId === this.userId;
@@ -214,17 +152,11 @@ export default {
   beforeMount() {
     this.getUser();
   },
-  // updated() {
-  //   console.log('UPDATS');
-  //   if (this.userId !== this.$route.params.userId) {
-  //     this.userId = this.$route.params.userId;
-  //   }
-  // },
   methods: {
     getUser() {
       this.loading = true;
       UserService.getUser(this.userId).then(
-          ({ data: user }) => {
+          ({data: user}) => {
             this.user = user;
             this.editedUser = {
               ...this.editedUser,
@@ -234,14 +166,21 @@ export default {
       ).finally(() => this.loading = false);
     },
     editUser() {
+      this.user = {
+        ...this.user,
+        ...this.editedUser,
+      };
+      this.currentUser.firstName = this.editedUser.firstName;
+      this.currentUser.lastName = this.editedUser.lastName;
       UserService.editUser(this.userId, this.editedUser)
           .then(() => {
-            const message = i18n.t('notifications.editUser-success');
+            const message = i18n.t('notifications.update-success');
             this.$store.dispatch('notification/showNotification', {
               message,
               type: NOTIFICATION_TYPES.SUCCESS,
             });
           });
+      this.editMode = !this.editMode;
     },
     cancelBtn() {
       this.editMode = !this.editMode;
@@ -255,10 +194,17 @@ export default {
     editedUser: {
       firstName: {
         required,
+        minLength: minLength(6),
+        maxLength: maxLength(50),
       },
-      password: {
+      lastName: {
         required,
+        minLength: minLength(6),
+        maxLength: maxLength(50),
       },
+      description: {
+        maxLength: maxLength(1200),
+      }
     },
   },
 };
